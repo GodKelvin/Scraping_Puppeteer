cffa4e2ef0f6974d292fcbf83bc2ec0100cbe314
GodKelvin <kelvin.lehrback@gmail.com>
2022-10-29 11:10:40 -0300

    implementando funcao de cleanDiv

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,43 @@
 import { launch } from 'puppeteer';
 import express, { Request, Response } from 'express';
 
+interface Produto{
+    nome: string,
+    preco: number,
+    qtdVendidos: number,
+    nomeLoja: string,
+    avaliacao: number
+}
+
 const server = express();
 
 const link = "https://pt.aliexpress.com/af/redmi-10-pro.html?d=y&origin=n&SearchText=redmi+10+pro&catId=0&spm=a2g0o.best.1000002.0&initiative_id=SB_20221028161423";
 
+
 server.get('/', async(_request: Request, response: Response) => {
     const browser = await launch();
     const page = await browser.newPage();
-    await page.goto(link);
+    await page.goto(link, {
+        waitUntil: "load",
+        timeout: 0
+    });
+    
     //Gera um print da pag acessada
     //await page.screenshot({path: "example.png"});
-    let teste = "pizza";
     const pageContent = await page.evaluate(() => {
-        return [...document.querySelectorAll("._3GR-w")].map((el: any) => el.querySelector(".ZzMrp")?.querySelector("._1kNf9")?.innerText);
+        const cleanDiv = (div: any, class1: String) => {
+            return div.querySelector(class1)?.innerText;
+        }
+        //leanDiv(el, "._18_85")
+
+        const divs = [...document.querySelectorAll("._3GR-w")]
+        return divs.map((el: any) => {
+            return {
+                nome: cleanDiv(el, "._18_85"),
+                qtdVendidos: cleanDiv(el, "._1kNf9")
+            }
+        });
+        //return [...document.querySelectorAll("._3GR-w")].map((el: any) => el.querySelector(".ZzMrp")?.querySelector("._1kNf9")?.innerText);
     });
     console.log(pageContent);
 
@@ -22,6 +46,8 @@ server.get('/', async(_request: Request, response: Response) => {
         "chave" : "Valor"
     });
 });
+
+
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Servidor running on port ${port}, acess: http://localhost:3000/`));
