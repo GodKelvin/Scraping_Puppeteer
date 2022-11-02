72a99744755d253b13ae61c783c2d3d7a4e3afbe
GodKelvin <kelvin.lehrback@gmail.com>
2022-11-02 17:50:50 -0300

    adc busca generica por query

--- src/controllers/aliExpress.ts
+++ src/controllers/aliExpress.ts
@@ -2,10 +2,12 @@ import { Request, Response } from "express"
 import { CrawlerAli } from '../models/Crawler';
 export const getAli = async(req: Request, res: Response): Promise<any> => {
     try{
-        let robozinho = new CrawlerAli("redmi 10 pro");
+        if(!req.query.search) return res.status(400).json({pendente: "params SEARCH nao foi identificado"}) ;
+        let robozinho = new CrawlerAli(String(req.query.search));
         let dataCrawler = await robozinho.run();
         return res.status(200).json(dataCrawler);
     }catch(error){
+        //@TODO inserir logs
         console.log(`ERROR: ${error}`);
         return res.status(500).json("Internal Server Error");
     }
